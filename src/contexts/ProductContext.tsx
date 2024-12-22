8186a400dc744dc0f9a5b5cb8aa0961adf0b23a6
mmd-rahimi <mohamad8rahimi3@gmail.com>
2024-12-22 10:51:26 +0330

    fething data

--- src/contexts/ProductContext.tsx
+++ src/contexts/ProductContext.tsx
@@ -1,6 +1,23 @@
-const ProductContext = () => {
-    return(
-        <h1></h1>
-    )
+import { createContext, useEffect, useState } from "react";
+
+export const ProductContext = createContext();
+
+const ProductProvider = ({children}) => {
+
+    const [products, setPrpducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const response = await fetch('https://fakestoreapi.com/products')
+            const data = await response.json();
+            console.log(data)
+        };
+        fetchProducts();
+    }, [])
+
+    return <ProductContext.Provider>
+        {children}
+    </ProductContext.Provider>
 }
-export default ProductContext;
+
+export default ProductProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import ReactDOM from "react-dom/client";
 import './index.css'
 import App from "./App";
+import ProductProvider from "./contexts/ProductContext";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+<>
+<ProductProvider />
+<App />
+</>
+);
