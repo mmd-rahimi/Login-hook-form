5de437cd5d665d93f5e0b4ff90b416cc909b2981
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-01-01 10:00:33 +0330

    starting mini shop PROJ to learn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { Route, BrowserRouter as Router } from "react-router";
 import { LoginForm } from "./pages/LoginForm";
+import { Routes } from "react-router";
+import { HomePage } from "./pages/HomePage";
 
 const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/Home" element={<HomePage />}/>
 
-    return(
-        <LoginForm />
-    )
-}
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
