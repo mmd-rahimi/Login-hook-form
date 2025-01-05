9a0e3155447281a9e41e885ebe49156f8b91106b
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-01-05 14:27:25 +0330

    fething products failed(need to fix)

--- src/Contexts/ProductContext.tsx
+++ src/Contexts/ProductContext.tsx
@@ -1,25 +1,23 @@
-import { createContext, useEffect, useState } from "react"
+import { createContext, useEffect, useState } from "react";
 
 export const ProductContext = createContext();
 
+export const ProductProvider = ({ children }) => {
+  const [Products, setProducts] = useState([]);
 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+    //   console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
 
-export const ProductProvider = ({children}) => {
-
-const [Products, setProducts] = useState([])
-
-useEffect(() => {
-const fetchProducts = async () => {
-    const response = await fetch('https://fakestoreapi.com/products');
-    const data = await response.json();
-    // console.log(data);
-    setProducts(data)
-}
-fetchProducts();
-}, [])
-
-    return <ProductContext.Provider value={{Products}}>
-        {children}
+  return (
+    <ProductContext.Provider value={{ Products }}>
+      {children}
     </ProductContext.Provider>
-    
-}
+  );
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,9 +5,16 @@ import { ProductCards } from "../components/ProductCards";
 import { ProductContext } from "../Contexts/ProductContext";
 
 export const HomePage = () => {
-  const  { products } = useContext(ProductContext);
+  const products = useContext(ProductContext);
   console.log(products);
-  
+
+//   const filteredProducts = products.filter((item) => {
+//     return (
+//       item.category === "men's clothing" || item.category === "women's clothing"
+//     );
+//   });
+// console.log(filteredProducts);
+
   return (
     <>
       <Header />
