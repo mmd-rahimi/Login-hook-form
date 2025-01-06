7c7cfaf3aeb6fb5db4e5a8e16c5485c55a2c8b81
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-01-06 13:11:08 +0330

    styling products almost completed

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -15,8 +15,8 @@ export const Product = ({ product }) => {
             </div>
         </div>
         {/*buttons*/}
-        <div className="absolute top-0 right-0 bg-slate-300 p-2">
-            <button><div className="flex justify-center items-center text-white w-12 h-12"><BsPlus className="text-3xl"/></div></button>
+        <div className="absolute top-6 right-11 group-hover:right-5  p-2 flex-col flex justify-center items-center opacity-0 group-hover:opacity-100 transition-all duration-300">
+            <button><div className="flex justify-center items-center text-white w-12 h-12 bg-slate-300/90"><BsPlus className="text-3xl"/></div></button>
             <Link to={'/'} className="w-12 h-12 bg-white flex justify-center items-center text-blue-950 drop-shadow-xl"><BsEyeFill /></Link>
         </div>
       </div>
