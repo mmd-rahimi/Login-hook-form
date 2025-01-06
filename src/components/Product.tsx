567a83eb2428ca6e8b55c7da6d4afe8dbf4ac6f2
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-01-06 12:21:37 +0330

    still styling products

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,3 +1,6 @@
+import { BsEyeFill, BsPlus } from "react-icons/bs";
+import { Link } from "react-router";
+
 export const Product = ({ product }) => {
   console.log(product);
 
@@ -8,8 +11,13 @@ export const Product = ({ product }) => {
         <div className="w-full h-full flex justify-center items-center">
             {/*image*/}
             <div className="w-[200px] flex mx-auto justify-center items-center">
-                <img className="max-h-[160px] group-hover:scale-110" src={image} alt={title} />
+                <img className="max-h-[160px] group-hover:scale-110 transition duration-300" src={image} alt={title} />
             </div>
+        </div>
+        {/*buttons*/}
+        <div className="absolute top-0 right-0 bg-slate-300 p-2">
+            <button><div className="flex justify-center items-center text-white w-12 h-12"><BsPlus className="text-3xl"/></div></button>
+            <Link to={'/'} className="w-12 h-12 bg-white flex justify-center items-center text-blue-950 drop-shadow-xl"><BsEyeFill /></Link>
         </div>
       </div>
       <div>2</div>
