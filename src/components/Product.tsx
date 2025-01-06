61e2d4f7ef1abbf4d90b25f91d8aefd2296aa2ef
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-01-06 12:08:11 +0330

    styling products

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,9 +1,18 @@
-export const Product = ({product}) => {
-    console.log(product);
-    
-    return (
-<div>
-Product
-</div>
-    )
-}
+export const Product = ({ product }) => {
+  console.log(product);
+
+  const { id, image, category, title, price } = product;
+  return (
+    <div>
+      <div className="border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition">
+        <div className="w-full h-full flex justify-center items-center">
+            {/*image*/}
+            <div className="w-[200px] flex mx-auto justify-center items-center">
+                <img className="max-h-[160px] group-hover:scale-110" src={image} alt={title} />
+            </div>
+        </div>
+      </div>
+      <div>2</div>
+    </div>
+  );
+};
