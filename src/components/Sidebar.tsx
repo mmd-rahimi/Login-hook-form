10ad46e9c83ed612829f2380546f21afa648b3fe
mmd-rahimi <mohamad8rahimi3@gmail.com>
2025-01-07 09:50:24 +0330

    sidebar almost completed

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -11,8 +11,9 @@ export const Sidebar = () => {
       }   w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
     >
       <div className="flex justify-between items-center py-6 border-b">
-        <div>Shopping Bag (0)</div>
-        <div className="cursor-pointer w-8 h-8 flex justify-center items-center">
+        <div className="uppercase text-sm font-semibold">Shopping Bag (0)</div>
+        {/* icon */}
+        <div onClick={handleClose} className="cursor-pointer w-8 h-8 flex justify-center items-center">
           <IoMdArrowForward className="text-2xl"/>
         </div>
       </div>
